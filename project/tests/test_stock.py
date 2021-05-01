33d42fcc969ce6e7952cad14ec3f6343ce319bc6
Alice Seaborn <seaborn.dev@gmail.com>
2021-05-01 13:32:32 -0600

    [NEW] Push tests and domain fixes to remote

--- project/tests/test_stock.py
+++ project/tests/test_stock.py
@@ -26,7 +26,7 @@ __author__ = "Austin Dial, Alice Seaborn"
 
 __version__ = "0.0.0"
 __maintainer__ = "Alice Seaborn"
-__email__ = "seaborn.archipelago@gmail.com"
+__email__ = "seaborn.dev@gmail.com"
 __status__ = "Prototype"
 
 
@@ -36,7 +36,7 @@ test_index_path = "project/tests/data/^INDX.csv"
 
 
 
-class TestInitialization(object):
+class Test_Initialization(object):
     
     def test_stock_type(self):
         
